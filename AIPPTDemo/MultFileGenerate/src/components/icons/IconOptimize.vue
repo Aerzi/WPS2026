f1ae9e858dcd36e8a0ec866780432a26b3ea72ae
xujiacheng <xujiacheng@wps.cn>
2025-11-03 17:22:01 +0800

    feat: 上传提示词优化界面

---
+++ AIPPTDemo/MultFileGenerate/src/components/icons/IconOptimize.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+    <path
+      d="M13.5 2.5L3.5 12.5M13.5 2.5L10.5 2.5M13.5 2.5V5.5"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path d="M2.5 13.5L4.5 11.5L6.5 13.5L4.5 15.5L2.5 13.5Z" fill="currentColor" />
+    <path
+      d="M8 6L9 7M6 8L7 9M10 4L11 5"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
